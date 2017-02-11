9443f194dbd0a0c20fdc1af1bf38ee5b7c47a78a
Ralph Segert <git@segert.net>
2017-02-11 13:58:23 +0100

    Update image_lib_lang.php

--- system/user/language/deutsch/image_lib_lang.php
+++ system/user/language/deutsch/image_lib_lang.php
@@ -2,9 +2,9 @@
 
 $lang = array(
 
-  'imglib_gd_required_for_props' => 'Zur Bestimmung der Bildeigenschaften muss der Server die GD Grafikbibliothek unterstützen',
+  'imglib_gd_required_for_props' => 'Zur Bestimmung der Bildeigenschaften muss der Server die GD-Grafikbibliothek unterstützen',
 
-  'imglib_unsupported_imagecreate' => 'Der Server unterstützt die GD Funktionen für die Bearbeitung dieser Bild-Typen nicht.',
+  'imglib_unsupported_imagecreate' => 'Der Server unterstützt die GD-Funktionen für die Bearbeitung dieser Bild-Typen nicht.',
 
   'imglib_gif_not_supported' => 'GIF-Bilder werden aus Lizenzgründen oft nicht unterstützt. Versuchen Sie es stattdessen mit dem JPG oder PNG-Format.',
 
@@ -12,7 +12,7 @@ $lang = array(
 
   'imglib_png_not_supported' => 'PNG-Bilder werden nicht unterstützt',
 
-  'imglib_jpg_or_png_required' => 'Das eingestellte Protokoll zur Änderung der Bildgrößen verarbeitet nur JPG oder PNG Bild-Typen',
+  'imglib_jpg_or_png_required' => 'Das eingestellte Protokoll zur Änderung der Bildgrößen verarbeitet nur JPG- oder PNG-Bilder',
 
   'imglib_copy_error' => 'Beim Versuch, die Datei zu ersetzen, ist ein Fehler aufgetreten. Bitte sorgen Sie für korrekte Schreibrechte in Ihrem Bild-Verzeichnis.',
 
@@ -24,11 +24,11 @@ $lang = array(
 
   'imglib_rotation_angle_required' => 'Sie müssen einen Winkel zum Drehen angeben.',
 
-  'imglib_writing_failed_gif' => 'GIF Bild',
+  'imglib_writing_failed_gif' => 'GIF-Bild',
 
   'imglib_invalid_path' => 'Der Pfad zu Ihrem Bild ist nicht korrekt',
 
-  'imglib_copy_failed' => 'Die Kopierroutine Bildes ist fehlgeschlagen.',
+  'imglib_copy_failed' => 'Die Kopierroutine des Bildes ist fehlgeschlagen.',
 
 
 // IGNORE
