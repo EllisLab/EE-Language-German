a5c59351b84c797ad97b8a362128ca0b757dd193
Ralph Segert <git@segert.net>
2017-02-11 14:10:13 +0100

    Update logs_lang.php

--- system/user/language/deutsch/logs_lang.php
+++ system/user/language/deutsch/logs_lang.php
@@ -2,7 +2,7 @@
 
 $lang = array(
 
-'system_logs'			=> 'System Logs',
+'system_logs'			=> 'System-Logs',
 
 /* Sidebar */
 'developer_log' 		=> 'Entwickler',
@@ -26,50 +26,50 @@ $lang = array(
 'logs_deleted'			=> 'Logs gelöscht',
 'logs_deleted_desc'		=> '<b>%d</b> Log(s) von <b>%s</b> Logs gelöscht',
 
-'clear_cp_logs'			=> 'Alle CP ZUgangs-Logs löschen',
+'clear_cp_logs'			=> 'Alle CP Zugangs-Logs löschen',
 
 'search_results' 		=> 'Suchresultate',
 'site_search' 			=> 'Site',
 'searched_in' 			=> 'Gesucht in',
-'search_terms' 			=> 'Suchterms',
+'search_terms' 			=> 'Suchbegriffe',
 
-'search_logs_button'	=> 'Such-logs',
+'search_logs_button'	=> 'Such-Logs',
 
 'date_logged'			=> 'Datum gelogged',
 
-'no_control_panel_logs_found' => 'Keine <b>Control Panel Logs</b> gefunden.',
+'no_control_panel_logs_found' => 'Keine <b>Control Panel-Logs</b> gefunden.',
 
 /* Developer Log */
 'no_developer_logs_found' => 'Keine <b>Entwickler-Logs</b> gefunden.',
 'clear_developer_logs'	=> 'Alle Entwickler-Logs gelöscht',
-'log_id'				=> 'Log ID',
-'log_message'			=> 'Nachricht',
+'log_id'		=> 'Log-ID',
+'log_message'		=> 'Nachricht',
 'deprecated_function'	=> 'Veraltete Funktion %s benutzt',
 'deprecated_on_line'	=> 'von %s in Zeile %d.',
 'deprecated_since'		=> 'Veraltet seit %s.',
 'deprecated_use_instead'	=> 'Stattdessen %s benutzen.',
-'deprecated_template'	=> 'Von Template Tag %s in %s.',
+'deprecated_template'	=> 'Von Template-Tag %s in %s.',
 'deprecated_snippets'	=> 'Dieser Tag wurde vielleicht von einem dieser Snippets benutzt: %s',
 'deprecated_hook'		=> 'Veralteter Extension Hook benutzt von den folgenden Extensions (Class::method) <ul>%s</ul>',
-'deprecation_detected'	=> 'Dies is ein Log für Entwickler um Mitteilungen oder Warnungen dem Super Admin zu melden.',
+'deprecation_detected'	=> 'Dies is ein Log für Entwickler, um Mitteilungen oder Warnungen dem Super-Admin zu melden.',
 'dev_log_view_report'	=> 'Report ansehen',
-'dev_log_help'			=> 'Was heisst das?',
+'dev_log_help'			=> 'Was heißt das?',
 'deprecated_meaning'	=>
 '<p>Eine der zwei Sachen ist passiert:</p>
 <ol>
-	<li>Das System hat nativen Code oder ein Add-on entdeckt, das veralteten Code benutzt, der nicht mehr funktioninert oder das System beeinträchtigt. Das bedeutet, dass ein Add-On in dieser Installation einen Teil des ExpressionEngine Core Code benutzt, den EllisLab in Zukunft plant aus dem System zu entfernen.</li>
-	<li>Eine Warnung oder Benachrichtigung wurde von nativem Code oder einem Add-On hervorgerufen, um dem Super Admin gemeldet zu werden.</li>
+	<li>Das System hat nativen Code oder ein Add-on entdeckt, das veralteten Code benutzt, der nicht mehr funktioninert oder das System beeinträchtigt. Das bedeutet, dass ein Add-On in dieser Installation einen Teil des ExpressionEngine Core-Codes benutzt, den EllisLab plant, in Zukunft aus dem System zu entfernen.</li>
+	<li>Eine Warnung oder Benachrichtigung wurde von nativem Code oder einem Add-On hervorgerufen, um dem Super-Admin gemeldet zu werden.</li>
 </ol>
 <p>Um diese Benachrichtung abzuschalten, befolgen Sie einen der folgenden Punkte:</p>
 <ul>
-	<li>Den Entwickler, der sich um das System kümmert, um Hilfe bitten</li>
-	<li>Sicherstellen, dass überall die neusten Versionen aller installierten Add-Ons laufen</li>
-	<li>Den oder die Entwickler des betreffenden Add-Ons kontaktieren um sicherzustellen, dass sie sich des Problems bewusst sind und sich um die Behebung des Problems bemühen</li>
+	<li>Den Entwickler, der sich um das System kümmert, um Hilfe bitten.</li>
+	<li>Sicherstellen, daß überall die neusten Versionen aller installierten Add-Ons laufen.</li>
+	<li>Den oder die Entwickler des betreffenden Add-Ons kontaktieren, um sicherzustellen, dass sie sich des Problems bewusst sind und sich um die Behebung des Problems bemühen.</li>
 </ul>',
 
 /* Email Log */
-'no_email_logs_found'	=> 'Kein <b>Email-Logs</b> gefunden.',
-'sent_to'				=> 'Gesendete E-Mail an',
+'no_email_logs_found'	=> 'Keine <b>Email-Logs</b> gefunden.',
+'sent_to'				=> 'Gesendete Email an',
 'subject'				=> 'Subject',
 'clear_email_logs'		=> 'Lösche alle Email-Logs',
 
@@ -82,7 +82,7 @@ $lang = array(
 'no_throttling_logs_found'	=> 'Keine <b>Throttling-Logs</b> gefunden.',
 'enable_throttling'		=> 'Throttling anschalten',
 'throttling_disabled'	=> '<b>Throttling</b> abgeschaltet',
-'front_end_requests'	=> 'Front End Anfragen',
+'front_end_requests'	=> 'Frontend-Anfragen',
 'last_activity' 		=> 'Letzte Aktivität',
 'no_throttle_logs' 		=> 'Keine IPs werden zur Zeit vom System beschränkt.',
 'blacklist_all_ips'		=> 'Alle IPs auf die schwarze Liste setzen',
