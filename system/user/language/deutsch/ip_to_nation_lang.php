39b90f1587ac6a7dd91585181ca7b00d9fae9918
Ralph Segert <git@segert.net>
2017-02-11 14:02:27 +0100

    Update ip_to_nation_lang.php

--- system/user/language/deutsch/ip_to_nation_lang.php
+++ system/user/language/deutsch/ip_to_nation_lang.php
@@ -2,7 +2,7 @@
 $lang = array(
 
 
-'ip_to_nation_module_name' => 'IP zu Nation',
+'ip_to_nation_module_name' => 'IP zur Nation',
 
 'ip_to_nation_module_description' => 'Programm zur Länderzuweisung von IP-Addressen',
 
@@ -10,9 +10,9 @@ $lang = array(
 
 'countryfile_missing' => 'Datei country.php im Ordner lib nicht gefunden.',
 
-'ip_search' => 'IP Adressen Suche',
+'ip_search' => 'IP-Adressen-Suche',
 
-'ip_search_inst' => 'IP Adresse eingeben, für welche das betreffende Land bestimmt werden soll.',
+'ip_search_inst' => 'IP-Adresse eingeben, die für das betreffende Land bestimmt werden soll.',
 
 'ip_result' => 'Die eingegebene IP gehört zu folgendem Land:',
 
@@ -30,15 +30,15 @@ $lang = array(
 
 'ban' => 'Sperren',
 
-'banlist' => 'Nationen Sperrliste',
+'banlist' => 'Nationen-Sperrliste',
 
 'banlist_updated' => 'Sperrliste erfolgreich nachgeführt',
 
-'update_ips' => 'IP Datenbank updaten',
+'update_ips' => 'IP-Datenbank updaten',
 
-'update_info' => 'IP und Ländercodeinformationen mit dem aktuellen Export von <a href="%d" title="Download hier">ip2nation.com</a> updaten',
+'update_info' => 'IP- und Ländercodeinformationen mit dem aktuellen Export von <a href="%d" title="Download hier">ip2nation.com</a> updaten',
 
-'update_blurb' => 'Für ein Update muss die ungezippte Datei mit den ip2nation sql Daten auf den Server gelegt werden und dann der Ort der Datei angegeben werden.',
+'update_blurb' => 'Für ein Update muss die ungezippte Datei mit den ip2nation-sql-Daten auf den Server gelegt werden und dann der Ort der Datei angegeben werden.',
 
 'update_data_provider' => 'Datenquelle: <a href="maxmind.com">maxmind.com</a>',
 
@@ -46,17 +46,17 @@ $lang = array(
 
 'ip_db_updating' => 'Download des Updates',
 
-'ip_db_downloaded' => 'IP Datenbank heruntergeladen. Entpacke ...',
+'ip_db_downloaded' => 'IP-Datenbank heruntergeladen. Entpacke ...',
 
-'ip_db_unpacked' => 'IP Datenbank entpackt. Füge Daten ein ...',
+'ip_db_unpacked' => 'IP-Datenbank entpackt. Füge Daten ein ...',
 
-'ip_db_updated' => 'IP-Datenbank update erfolgreich',
+'ip_db_updated' => 'IP-Datenbank-Update erfolgreich',
 
 'ip_db_failed' => 'Unerlaubte Zeichen in der Import-Datei',
 
-'ip_db_connection_403' => '403 Verbindungsfehler, bitte 24 Stunden warten bevor die Datenbank erneut abgefragt wird.',
+'ip_db_connection_403' => '403 Verbindungsfehler: Bitte 24 Stunden warten, bevor die Datenbank erneut abgefragt wird.',
 
-'ip_db_connection_error' => 'Kann nicht mit dem Daten-Provider verbinden.',
+'ip_db_connection_error' => 'Keine Verbindung mit dem Daten-Provider möglich.',
 
 'translate' => 'Update',
 
