c8b07bf9f97f1d7a6eee36a1856f39fb0563fc0f
Ralph Segert <git@segert.net>
2017-02-12 19:23:16 +0100

    Update specialty_tmp_lang.php

--- system/user/language/deutsch/specialty_tmp_lang.php
+++ system/user/language/deutsch/specialty_tmp_lang.php
@@ -4,51 +4,51 @@ $lang = array(
 
 'admin_notify_mailinglist' => 'Admin-Benachrichtigung bei Mailinglisten-Eintrag',
 
-'admin_notify_mailinglist_desc' => 'Dieses Template enthält die Mitteilung, die ein Admin erhält, wenn sich jemand in eine Mailingliste einträgt. ',
+'admin_notify_mailinglist_desc' => 'Dieses Template enthält die Mitteilung, die ein Admin bekommt, wenn sich jemand in eine Mailingliste einträgt. ',
 
-'admin_notify_forum_post_desc' => 'Dieses Template enthält die Mitteilung, die ein Admin bei einem neuen Forumsbeitrag erhält.',
+'admin_notify_forum_post_desc' => 'Dieses Template enthält die Mitteilung, die ein Admin bei einem neuen Forumsbeitrag bekommt.',
 
 'admin_notify_entry' => 'Admin-Benachrichtigung bei neuen Einträgen',
 
-'admin_notify_entry_desc' => 'Dieses Template enthält die Nachricht die Empfänger erhalten, wenn neue Einträge verfasst wurden.',
+'admin_notify_entry_desc' => 'Dieses Template enthält die Nachricht, die Empfänger erhalten, wenn neue Einträge verfasst wurden.',
 
 'private_message_notification' => 'Benachrichtigung bei privaten Nachrichten',
 
-'private_message_notification_desc' => 'Dieses Template enthält die Nachricht, die das System bei Erhalt einer privaten Nachricht an den User versendet',
+'private_message_notification_desc' => 'Dieses Template enthält die Nachricht, die das System bei Erhalt einer privaten Nachricht an das Mitglied versendet',
 
-'forum_post_notification_desc' => 'Dieses Template enthält die Nachricht, die das System bei der Antwort auf einen Forum-Beitrag eines Users versendet',
+'forum_post_notification_desc' => 'Dieses Template enthält die Nachricht, die das System bei der Antwort auf einen Forum-Beitrag eines Mitglieds versendet',
 
 'admin_notify_forum_post' => 'Forum-Posting-Benachrichtigung für Admins',
 
 'forum_post_notification' => 'Benutzer-Benachrichtigung bei Forum-Postings',
 
-'email_title' => 'E-Mail-Titel',
+'email_title' => 'Email-Titel',
 
-'email_message' => 'E-Mail-Nachricht',
+'email_message' => 'Email-Nachricht',
 
 'admin_notify_comment' => 'Kommentar-Benachrichtigung für Admins',
 
 'admin_notify_comment_desc' => 'Dieses Template enthält die Nachricht, die ein Administrator beim Speichern eines Kommentars erhält.',
 
-'admin_notify_reg' => 'Admin-Benachrichtigung bei Neuanmeldung eines Users',
+'admin_notify_reg' => 'Admin-Benachrichtigung bei Neuanmeldung eines Mitglieds',
 
 'admin_notify_reg_desc' => 'Dieses Template enthält die Nachricht, die ein Administrator bei der Anmeldung eines neuen Mitglieds erhält.',
 
-'comment_notification' => 'Benachrichtigung des Users bei Kommentaren',
+'comment_notification' => 'Benachrichtigung des Mitglieds bei Kommentaren',
 
-'comments_opened_notification' => 'User Kommentar angefügt Benachrichtigung',
+'comments_opened_notification' => 'Mitglieder-Kommentar angefügt: Benachrichtigung',
 
-'comment_notification_desc' => 'Dieses Template enth&auml;lt die Nachricht, die ein User erhält, wenn jemand auf seinen Kommentar geantwortet hat.',
+'comment_notification_desc' => 'Dieses Template enth&auml;lt die Nachricht, die ein Mitglied erhält, wenn jemand auf seinen Kommentar geantwortet hat.',
 
 'forgot_password_instructions' => 'Anweisungen für verlorenes Passwort',
 
-'forgot_password_instructions_desc' => 'Dieses Template enthält die Anweisungen, die ein Benutzer bekommt, wenn er sein Passwort vergessen hat.',
+'forgot_password_instructions_desc' => 'Dieses Template enthält die Anweisungen, die ein Mitglied bekommt, wenn er sein Passwort vergessen hat.',
 
-'mailinglist_activation_instructions' => 'Anweisungen für die Aktivierung der Mailing-Liste',
+'mailinglist_activation_instructions' => 'Anweisungen für die Aktivierung der Mailingliste',
 
-'mailinglist_activation_instructions_desc' => 'Dieses Template enthält die Anweisungen zur Aktivierung in der Mailing-Liste.',
+'mailinglist_activation_instructions_desc' => 'Dieses Template enthält die Anweisungen zur Aktivierung in der Mailingliste.',
 
-'mbr_activation_instructions' => 'Anweisungen für die Aktivierung des Nutzer-Kontos',
+'mbr_activation_instructions' => 'Anweisungen für die Aktivierung des Mitglieder-Kontos',
 
 'mbr_activation_instructions_desc' => 'Dieses Template enthält die Anweisungen zur Aktivierung der Mitgliedschaft bei einer neuen Registrierung',
 
@@ -70,21 +70,21 @@ $lang = array(
 
 'notification_has_char_limit' => 'Die meisten oben genannten Variablen akzeptieren einen char_limit Parameter (title char_limit="40"}',
 
-'pm_inbox_full' => 'Benachrichtigung bei voller InBox bei Privaten Nachrichten',
+'pm_inbox_full' => 'Benachrichtigung bei voller Inbox bei privaten Nachrichten',
 
-'pm_inbox_full_desc' => 'Dieses Template enthält die Mitteilung für User mit voller InBox bei den Privaten Nachrichten, da sie sonst keine mehr erhalten werden. ',
+'pm_inbox_full_desc' => 'Dieses Template enthält die Mitteilung für Mitglieder mit voller Inbox für die privaten Nachrichten, da sie sonst keine mehr erhalten werden. ',
 
-'forum_moderation_notification' => 'Benachrichtigung an Benutzer wegen Forum-Moderation',
+'forum_moderation_notification' => 'Benachrichtigung an Mitglieder wegen einer Forums-Moderation',
 
 'forum_moderation_notification_desc' => 'Dieses Template enthält die Mitteilung für Benutzer, deren Forums-Beitrag von einem Moderator verschoben, aufgeteilt oder zusammengefügt wurde',
 
-'forum_report_notification' => 'Moderator-Benachrichtigung für Forum-Rapporte',
+'forum_report_notification' => 'Moderator-Benachrichtigung für Forums-Rapporte',
 
-'forum_report_notification_desc' => 'Dieses Template enthält die Benachrichtigung fär Moderatoren wenn Besucher Forumsbeträge rapportieren',
+'forum_report_notification_desc' => 'Dieses Template enthält die Benachrichtigung für Moderatoren, wenn Besucher Forumsbeträge rapportieren',
 
-'email_subject' => 'E-Mail Betreff',
+'email_subject' => 'Email-Betreff',
 
-'message_body' => 'E-Mail Textteil',
+'message_body' => 'Email-Textteil',
 
 'translate' => 'Update',
 
