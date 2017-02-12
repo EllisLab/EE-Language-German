73f2981411b275f8a7563585d93e0972417a4e87
Ralph Segert <git@segert.net>
2017-02-12 18:02:39 +0100

    Update profiler_lang.php

--- system/user/language/deutsch/profiler_lang.php
+++ system/user/language/deutsch/profiler_lang.php
@@ -1,20 +1,20 @@
 <?php
 
-$lang['profiler_load']                 = 'Load';
+$lang['profiler_load']                 = 'Lade';
 $lang['profiler_performance']          = 'Performance';
-$lang['profiler_memory']               = 'Memory Usage';
-$lang['profiler_no_memory']            = 'Memory Usage Unavailable';
-$lang['profiler_variables']            = 'Variables';
+$lang['profiler_memory']               = 'Speichernutzung';
+$lang['profiler_no_memory']            = 'Speichernutzung nicht verfügbar';
+$lang['profiler_variables']            = 'Variablen';
 $lang['profiler_server']               = '$_SERVER';
 $lang['profiler_cookie']               = '$_COOKIE';
 $lang['profiler_get']                  = '$_GET';
 $lang['profiler_post']                 = '$_POST';
-$lang['profiler_userdata']             = 'Userdata';
-$lang['profiler_no_variables']         = 'No <code><b>%s</b></code> variables found.';
-$lang['profiler_queries']              = 'Queries';
-$lang['profiler_duplicate_queries']    = 'Duplicate Queries';
-$lang['profiler_no_duplicate_queries'] = 'No duplicate queries.';
-$lang['profiler_no_queries']           = 'No queries were run';
+$lang['profiler_userdata']             = 'Benutzerdaten';
+$lang['profiler_no_variables']         = 'Keine <code><b>%s</b></code> Variablen gefunden.';
+$lang['profiler_queries']              = 'Abfragen';
+$lang['profiler_duplicate_queries']    = 'Duplizierte Abfragen';
+$lang['profiler_no_duplicate_queries'] = 'Keine duplizierten Abfragen.';
+$lang['profiler_no_queries']           = 'Es wurden keine Abfragen ausgeführt';
 $lang['profiler_template']             = 'Templates';
 $lang['profiler_gigabytes']            = 'GB';
 $lang['profiler_megabytes']            = 'MB';
