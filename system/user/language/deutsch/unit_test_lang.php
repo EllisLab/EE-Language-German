050e402ae97e34529e1ecc871d671fda5905a853
Ralph Segert <git@segert.net>
2017-02-12 20:05:58 +0100

    Update unit_test_lang.php

--- system/user/language/deutsch/unit_test_lang.php
+++ system/user/language/deutsch/unit_test_lang.php
@@ -1,14 +1,14 @@
 <?php
 
 $lang['ut_test_name']		= 'Test Name';
-$lang['ut_test_datatype']	= 'Test Datatype';
-$lang['ut_res_datatype']	= 'Expected Datatype';
-$lang['ut_result']			= 'Result';
-$lang['ut_undefined']		= 'Undefined Test Name';
-$lang['ut_file']			= 'File Name';
-$lang['ut_line']			= 'Line Number';
-$lang['ut_passed']			= 'Passed';
-$lang['ut_failed']			= 'Failed';
+$lang['ut_test_datatype']	= 'Test Datentyp';
+$lang['ut_res_datatype']	= 'Erwarteter Datentyp';
+$lang['ut_result']			= 'Resultat';
+$lang['ut_undefined']		= 'Undefinierter Test Name';
+$lang['ut_file']			= 'Dateiname';
+$lang['ut_line']			= 'Zeilennummer';
+$lang['ut_passed']			= 'Bestanden';
+$lang['ut_failed']			= 'Fehlgeschlagen';
 $lang['ut_boolean']			= 'Boolean';
 $lang['ut_integer']			= 'Integer';
 $lang['ut_float']			= 'Float';
@@ -16,7 +16,7 @@ $lang['ut_double']			= 'Float'; // can be the same as float
 $lang['ut_string']			= 'String';
 $lang['ut_array']			= 'Array';
 $lang['ut_object']			= 'Object';
-$lang['ut_resource']		= 'Resource';
+$lang['ut_resource']		= 'Ressource';
 $lang['ut_null']			= 'Null';
 $lang['ut_notes']			= 'Notes';
 
