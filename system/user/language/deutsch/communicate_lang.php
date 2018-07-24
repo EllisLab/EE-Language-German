48199ebb623f2010b93c541691a95d2b3821f3b5
Ralph Segert <git@segert.net>
2018-07-24 10:43:40 +0200

    Update communicate_lang.php
    Translated to version 4.3.2

--- system/user/language/deutsch/communicate_lang.php
+++ system/user/language/deutsch/communicate_lang.php
@@ -1,82 +1,153 @@
 <?php
+
 $lang = array(
 
-  'your_name' => 'Ihr Name',
-  'your_email' => 'Ihre Email-Adresse',
-  'recipient' => 'Empfänger',
-  'cc' => 'CC',
-  'bcc' => 'BCC',
-  'recipient_group' => 'An Nutzer-Gruppen versenden',
-  'manual_recipients_ignored' => 'Das obige Empfängerfeld wird ignoriert',
-  'honor_email_pref' => 'Nur an Mitglieder versenden, die sich bereit erklärt haben, Emails zu akzeptieren.',
-  'separate_emails_with_comma' => 'Mehrere Empfänger durch Kommas trennen',
-  'no_email_matching_criteria' => 'Keine übereinstimmenden Email-Adressen gefunden',
-  'not_allowed_to_email_members' => 'Sie dürfen keine Email an Mitglieder versenden',
-  'not_allowed_to_email_member_groups' => 'Sie dürfen keine Email an Nutzer-Gruppen versenden',
-  'not_allowed_to_email_cache' => 'Sie dürfen keine Einsicht in das Email-Archiv nehmen.',
-  'subject' => 'Betreff',
-  'message' => 'Mitteilung',
-  'plaintext_body'		=> 'Reiner Text',
-  'plaintext_alt'			=> 'Nur-Text-Alternative, nur für <abbr title="Hyper-Text Markup Language">HTML</abbr>-Emails, wenn das Programm des Empfängers kein <abbr title="Hyper-Text Markup Language">HTML</abbr> interpretieren kann.',
-  'send_an_email' => 'Email senden',
-  'sending_email' => 'Versende Email',
-  'batchmode_ready_to_begin' => 'Die Emails werden in fünf Sekunden versendet',
-  'batchmode_warning' => 'Lassen Sie Ihren Browser in Ruhe, bis der Prozess abgeschlossen ist!',
-  'batchmode_disabled'	=> 'Batch-Mode ist abgeschaltet',
-  'problem_with_id' => 'Ein Problem mit der für den Email-Versand nötigen ID ist aufgetreten.',
-  'cache_data_missing' => 'Email Cache-Daten wurden nicht gefunden.',
-  'currently_sending_batch' => 'Sende Emails %x bis %y',
-  'emails_remaining' => 'Verbleibende Emails:',
-  'email_error' => 'Email Fehler',
-  'send_it' => 'Senden',
-  'total_emails_sent' => 'Anzahl der versendeten Emails:',
-  'plain_text' => 'Nur Text',
-  'html' => 'HTML',
-  'mail_format' => 'Email-Formatierung',
-  'text_formatting' => 'Text-Formatierung',
-  'none' => 'Keine',
-  'auto_br' => 'Auto <br />',
-  'xhtml' => 'XHTML',
-  'wordwrap' => 'Wortumbruch',
-  'priority' => 'Priorität',
-  'attachment' => 'Anhang',
-  'attachment_problem' => 'Es gab ein Problem, die Datei anzuhängen.',
-  'attachment_unavailable' => 'Um Anhänge zu versenden, muss ein Upload-Ordner definiert sein.',
-  'attachment_warning' => 'Anhänge werden von ExpressionEngine <strong>nicht gespeichert</strong>. Diese müssen lokal gespeichert sein.',
-  'chars' => 'Zeichen',
-  'highest' => 'Höchste',
-  'high' => 'Hoch',
-  'normal' => 'Normal',
-  'low' => 'Niedrig',
-  'lowest' => 'Sehr niedrig',
-  'empty_form_fields' => 'Sie haben nicht alle Felder ausgefüllt',
-  'email_sent_message' => 'Ihre Nachricht wurde gesendet',
-  'all_email_sent_message' => 'Alle Nachrichten wurden gesendet',
-  'email_success' => 'Email gesendet',
-  'view_email_cache' => 'Früher gesendete Emails ansehen',
-  'emails'				=> 'Emails',
-  'previous_email' => 'Früher gesendete Emails',
-  'email_title' => 'Email Titel',
-  'email_date' => 'Sendedatum',
-  'total_recipients' => 'Anzahl der Empfänger',
-  'resend' => 'Nochmals senden',
-  'view' => 'Zeigen',
-  'no_cached_email' => 'Es sind keine Emails gecachet',
-  'delete_emails' => 'Email löschen',
-  'delete_confirm' => 'Email Konfiguration löschen',
-  'delete_question' => 'Sind Sie sicher, dass Sie diese Email(s) löschen wollen?',
-  'email_deleted'			=> 'Email wurde gelöscht',
-  'complete' => 'Erledigt',
-  'incomplete' => 'Unerledigt',
-  'finish_sending' => 'Senden beendet',
-  'on' => 'Ein ',
-  'off' => 'Aus',
-  'total_sent'			=> 'Insgesamt gesendet',
-  'remove'				=> 'Entfernen',
-  'dummy_location'		=> 'Die Email wurde an: %s gesendet',
-  'search_emails_button'	=> 'Emails durchsuchen',
+'all_email_sent_message' => 'Alle Nachrichten wurden gesendet',
 
-  ''=>''
+'attachment' => 'Anhang',
+
+'attachment_problem' => 'Es gab ein Problem mit dem Anhängen der Datei.',
+
+'attachment_unavailable' => 'Um Anhänge zu versenden, muss ein Upload-Ordner definiert sein.',
+
+'attachment_warning' => 'Anhänge werden von ExpressionEngine <strong>nicht gespeichert</strong>. Diese müssen lokal gespeichert sein.',
+
+'batchmode_disabled' => 'Batch-Mode ist abgeschaltet',
+
+'batchmode_ready_to_begin' => 'Die Emails werden in fünf Sekunden versendet',
+
+'batchmode_warning' => 'Lassen Sie Ihren Browser in Ruhe, bis der Prozess abgeschlossen ist!',
+
+'batch_size_is_zero' => 'Stapelmodus aktiviert, aber die Stapelgröße ist nicht richtig eingestellt. Überprüfen Sie Ihre <a href="'.DOC_URL.'/general/system_configuration_overrides.html#email-batch-size" rel="externe"> Email-Konfigurationsüberschreibungen </a> in Ihrer Konfigurationsdatei.',
+
+'bcc' => 'BCC',
+
+'cache_data_missing' => 'Email Cache-Daten wurden nicht gefunden.',
+
+'cc' => 'CC',
+
+'chars' => 'Zeichen',
+
+'complete' => 'Komplett',
+
+'currently_sending_batch' => 'Sende Emails %x bis %y',
+
+'delete_confirm' => 'Email-Konfiguration löschen',
+
+'delete_emails' => 'Email löschen',
+
+'delete_question' => 'Sind Sie sicher, dass Sie diese Email(s) löschen wollen?',
+
+'dummy_location' => 'Die Email wurde an %s gesendet',
+
+'email_date' => 'Sendedatum',
+
+'email_deleted' => 'Email wurde gelöscht',
+
+'email_error' => 'Emailfehler',
+
+'email_sent_message' => 'Ihre Nachricht wurde gesendet',
+
+'email_success' => 'Email gesendet',
+
+'email_title' => 'Email-Titel',
+
+'emails' => 'Emails',
+
+'emails_remaining' => 'Verbleibende Emails:',
+
+'empty_form_fields' => 'Sie haben nicht alle Felder ausgefüllt.',
+
+'finish_sending' => 'Senden beendet',
+
+'high' => 'Hoch',
+
+'highest' => 'Höchste',
+
+'honor_email_pref' => 'Nur an Mitglieder versenden, die sich bereit erklärt haben, Emails zu akzeptieren.',
+
+'html' => 'HTML',
+
+'incomplete' => 'Nicht komplett',
+
+'low' => 'Niedrig',
+
+'lowest' => 'Niedrigste',
+
+'mail_format' => 'Emailformatierung',
+
+'manual_recipients_ignored' => 'Das obige Empfängerfeld wird ignoriert',
+
+'markdown' => 'Markdown',
+
+'message' => 'Mitteilung',
+
+'no_cached_email' => 'Keine <b>cached Emails</b> gefunden.',
+
+'no_email_matching_criteria' => 'Keine übereinstimmenden Email-Adressen gefunden',
+
+'normal' => 'Normal',
+
+'not_allowed_to_email_cache' => 'Sie dürfen keine Einsicht in das Email-Archiv nehmen.',
+
+'not_allowed_to_email_member_groups' => 'Sie dürfen keine Email an Nutzer-Gruppen versenden',
+
+'not_allowed_to_email_members' => 'Sie dürfen keine Email an Mitglieder versenden',
+
+'off' => 'Aus',
+
+'on' => 'An',
+
+'plain_text' => 'Nur Text',
+
+'plaintext_alt' => 'Reiner-Text-Alternative: Nur für <abbr title="Hyper-Text Markup Language">HTML</abbr>-Emails, wenn das Programm des Empfängers kein <abbr title="Hyper-Text Markup Language">HTML</abbr> interpretieren kann.',
+
+'plaintext_body' => 'Reiner Text',
+
+'previous_email' => 'Zuvor gesendete E-Mail',
+
+'priority' => 'Priorität',
+
+'problem_with_id' => 'Ein Problem mit der für den Email-Versand nötigen ID ist aufgetreten.',
+
+'recipient' => 'Empfänger',
+
+'recipient_group' => 'Sende zur Mitgliedergruppe',
+
+'remove' => 'Entfernen',
+
+'resend' => 'Nochmals senden',
+
+'search_emails_button' => 'Emails durchsuchen',
+
+'send_an_email' => 'Email senden',
+
+'send_it' => 'Senden',
+
+'sending_email' => 'Versende Email',
+
+'separate_emails_with_comma' => 'Mehrere Empfänger durch Kommas trennen',
+
+'subject' => 'Befreff',
+
+'text_formatting' => 'Textformatierung',
+
+'total_emails_sent' => 'Anzahl der versendeten Emails:',
+
+'total_recipients' => 'Anzahl der Empfänger',
+
+'total_sent' => 'Insgesamt gesendet',
+
+'view' => 'Zeigen',
+
+'view_email_cache' => 'Früher gesendete Emails ansehen',
+
+'wordwrap' => 'Wordumbruch',
+
+'your_email' => 'Deine Emailadresse',
+
+'your_name' => 'Dein Name',
+
 );
-/* End of file communicate_lang.php */
-/* Location: ./system/user/language/deutsch/communicate_lang.php */
+
+// EOF
