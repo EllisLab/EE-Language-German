5c302b56e748f9f5736fca060288c4dd44f2c91a
Ralph Segert <git@segert.net>
2018-07-24 10:58:48 +0200

    Update expressionengine_info_lang.php
    Translated to version 4.3.2

--- system/user/language/deutsch/expressionengine_info_lang.php
+++ system/user/language/deutsch/expressionengine_info_lang.php
@@ -2,31 +2,30 @@
 
 $lang = array(
 
-  'expressionengine_info' => 'ExpressionEngine-Info',
+'current_version' => '&nbsp;&nbsp;&nbsp;Aktuell: %v',
 
-  'resources' => 'Ressourcen',
-  'documentation' => 'Online-Dokumentation',
-  'support_resources' => 'Support-Ressourcen',
-  'downloads' => 'Meine Downloads im ExpressionEngine-Store',
+'documentation' => 'Dokumentation',
 
-  'running_current' => 'Sie arbeiten mit der aktuellen Version von ExpressionEngine: Version %v Build %b',
-  'version_update_available' => 'Eine neuere Version von ExpressionEngine ist erhältlich!',
-  'version_update_inst' => 'Bitte downloaden Sie die <a class="update" href="%d" title="Download hier" rel="external">aktuellste Version</a> und folgen Sie den <a class="update" href="%i" title="Update Instructions" rel="external">Update-Anweisungen</a></a>.',
+'downloads' => 'Meine Downloads im ExpressionEngine-Store',
 
-  /*
-  'build_update_available'	=> 'A newer build of ExpressionEngine is available!',
-  'build_update_inst'			=> 'Please download the <a class="update" href=\'%d\' title=\'Download Here\'>latest build</a> and follow the <a class="update" href=\'%i\' title=\'Build Update Instructions\'>Build Update Instructions</a>.',
-  */
+'error_getting_version' => 'Sie arbeiten mit ExpressionEngine %v. Ob eine aktuellere Version vorliegt, kann zur Zeit nicht ermittelt werden.',
 
-  'current_version' => '&nbsp;&nbsp;&nbsp;Aktuell: %v',
-  'installed_version' => 'Installiert: %v',
+'expressionengine_info' => 'ExpressionEngine-Info',
 
+'installed_version' => 'Installiert: %v',
 
-  'version_and_build' => 'Derzeitige Version',
-  'error_getting_version' => 'Sie arbeiten mit ExpressionEngine %v. Ob eine aktuellere Version vorliegt, kann zur Zeit nicht ermittelt werden.',
+'resources' => 'Ressourcen',
 
-  ''=>''
+'running_current' => 'Hier arbeitet die aktuelle Version von ExpressionEngine: %v Build %b',
+
+'support_resources' => 'Technischer Support',
+
+'version_and_build' => 'Derzeitige Version',
+
+'version_update_available' => 'Eine neuere Version von ExpressionEngine ist erhältlich!',
+
+'version_update_inst' => 'Bitte downloaden Sie die <a class="update" href="%d" title="Download hier" rel="external">aktuellste Version</a> und folgen Sie den <a class="update" href="%i" title="Update Instructions" rel="external">Update-Anweisungen</a></a>.',
+
 );
 
-/* End of file expressionengine_info_lang.php */
-/* Location: ./system/user/language/deutsch/expressionengine_info_lang.php */
+// EOF
