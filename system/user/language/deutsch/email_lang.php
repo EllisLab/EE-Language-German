167c039c29caed87b24fbad8191cadca0dbd9203
Ralph Segert <git@segert.net>
2018-07-24 10:56:47 +0200

    Update email_lang.php
    Translated to version 4.3.2

--- system/user/language/deutsch/email_lang.php
+++ system/user/language/deutsch/email_lang.php
@@ -2,59 +2,70 @@
 
 $lang = array(
 
-//----------------------------
-// Email
-//----------------------------
+'em_banned_from_email' => 'Die angegebene Sender-Email-Adresse ist gesperrt.',
 
-  'email_module_name' => 'Email',
+'em_banned_recipient' => 'Einer oder mehrere Empfänger-Email-Adressen sind gesperrt.',
 
-  'email_module_description' => 'User Email Modul',
+'em_email_sent' => 'Ihre Email-Mitteilung wurde gesendet.',
 
-  'message_required' => 'Email Mitteilung ist erforderlich',
+'em_interval_warning' => 'Sie haben nur alle %s Sekunden die Möglichkeit eine Email zu senden',
 
-  'em_banned_from_email' => 'Die angegebene Sender-Email-Adresse ist gesperrt.',
+'em_invalid_recipient' => 'Einer oder mehrere Empfänger-Email-Adressen sind ungültig.',
 
-  'em_banned_recipient' => 'Einer oder mehrere Empfänger-Email-Adressen sind gesperrt.',
+'em_limit_exceeded' => 'Sie haben die Grenze der erlaubten Anzahl Emails, die pro Tag versendet werden können, überschritten.',
 
-  'em_invalid_recipient' => 'Einer oder mehrere Empfänger-Email-Adressen sind ungültig.',
+'em_no_preview_template_specified' => 'Der Email-Tag enthält keinen Verweis auf das Template zur Voransicht.',
 
-  'em_no_valid_recipients' => 'Ihre Email hatte keinen gültigen Empfänger',
+'em_no_valid_recipients' => 'Ihre Email hatte keinen gültigen Empfänger',
 
-  'em_sender_required' => 'Eine gültige Sender-Email-Adresse ist nötig',
+'em_sender_required' => 'Eine gültige Sender-Email-Adresse ist nötig',
 
-  'em_unauthorized_request' => 'Sie haben keine Erlaubnis für diese Aktion',
+'em_unauthorized_request' => 'Sie haben keine Erlaubnis für diese Aktion',
 
-  'em_limit_exceeded' => 'Sie haben die Grenze der erlaubten Anzahl Emails, die pro Tag versendet werden können, überschritten.',
+'email_attachment_missing' => 'Der folgende Email-Anhang konnte nicht lokalisiert werden: %s',
 
-  'em_interval_warning' => 'Sie haben nur alle %s Sekunden die Möglichkeit eine Email zu senden',
+'email_attachment_unreadable' => 'Dieser Anhang kann nicht geöffnet werden: %s',
 
-  'em_email_sent' => 'Ihre Email-Mitteilung wurde gesendet.',
+'email_exit_status' => 'Exit Statuscode: %s',
 
-  'em_no_preview_template_specified' => 'Der Email-Tag enthält keinen Verweis auf das Template zur Voransicht.',
+'email_failed_smtp_login' => 'AUTH LOGIN Befehl senden ist fehlgeschlagen. Fehler: %s',
 
-  'email_must_be_array' => 'Die Email-Validierungsmethode muss in einem Array übergeben werden.',
-  'email_invalid_address' => 'Ungültige Email-Adresse: %s',
-  'email_attachment_missing' => 'Der folgende Email-Anhang konnte nicht lokalisiert werden: %s',
-  'email_attachment_unreadable' => 'Dieser Anhang kann nicht geöffnet werden: %s',
-  'email_no_recipients' => 'Sie müssen Empfänger angeben: An, Cc oder Bcc',
-  'email_send_failure_phpmail' => 'Kann Email nicht mit PHP mail () versenden. Ihr Server könnte für diese Versandmethode nicht konfiguriert sein.',
-  'email_send_failure_sendmail' => 'Kann Email nicht mit PHP Sendmail versenden. Ihr Server könnte für diese Versandmethode nicht konfiguriert sein.',
-  'email_send_failure_smtp' => 'Kann Email nicht mit PHP SMTP versenden. Ihr Server könnte für diese Versandmethode nicht konfiguriert sein.',
-  'email_sent' => 'Ihre Mitteilung wurde erfolgreich mit folgendem Protokoll versendet: %s',
-  'email_no_socket' => 'Kann socket zu Sendmail nicht öffnen. Bitte Einstellungen überprüfen.',
-  'email_no_hostname' => 'Es wurde kein SMTP-Hostname angegeben.',
-  'email_no_port' => 'Sie haben keinen SMTP-Port angegeben.',
-  'email_smtp_error' => 'Folgender SMTP-Fehler trat auf: %s',
-  'email_no_smtp_unpw' => 'Fehler: Es muss ein SMTP-Benutzername und Passwort angegeben werden.',
-  'email_failed_smtp_login' => 'AUTH LOGIN Befehl senden ist fehlgeschlagen. Fehler: %s',
-  'email_smtp_auth_un' => 'Authentifizierung des Benutzernamens fehlgeschlagen. Fehler: %s',
-  'email_smtp_auth_pw' => 'Authentifizierung des Passwortes fehlgeschlagen. Fehler: %s',
-  'email_smtp_data_failure' => 'Kann diese Daten nicht senden: %s',
-  'email_exit_status' => 'Exit Statuscode: %s',
+'email_invalid_address' => 'Ungültige Email-Adresse: %s',
 
+'email_module_description' => 'Benutzer-Email-Modul',
 
-  ''=>''
+'email_module_name' => 'Email',
+
+'email_must_be_array' => 'Die Email-Validierungsmethode muss in einem Array übergeben werden.',
+
+'email_no_hostname' => 'Es wurde kein SMTP-Hostname angegeben.',
+
+'email_no_port' => 'Sie haben keinen SMTP-Port angegeben.',
+
+'email_no_recipients' => 'Sie müssen Empfänger angeben: An, Cc oder Bcc',
+
+'email_no_smtp_unpw' => 'Fehler: Es muss ein SMTP-Benutzername und Passwort angegeben werden.',
+
+'email_no_socket' => 'Kann Socket zu Sendmail nicht öffnen. Bitte Einstellungen überprüfen.',
+
+'email_send_failure_phpmail' => 'Kann Email nicht mit PHP mail () versenden. Ihr Server könnte für diese Versandmethode nicht konfiguriert sein.',
+
+'email_send_failure_sendmail' => 'Kann Email nicht mit PHP Sendmail versenden. Ihr Server könnte für diese Versandmethode nicht konfiguriert sein.',
+
+'email_send_failure_smtp' => 'Kann Email nicht mit PHP SMTP versenden. Ihr Server könnte für diese Versandmethode nicht konfiguriert sein.',
+
+'email_sent' => 'Ihre Mitteilung wurde erfolgreich mit folgendem Protokoll versendet: %s',
+
+'email_smtp_auth_pw' => 'Authentifizierung des Passwortes fehlgeschlagen. Fehler: %s',
+
+'email_smtp_auth_un' => 'Authentifizierung des Benutzernamens fehlgeschlagen. Fehler: %s',
+
+'email_smtp_data_failure' => 'Kann diese Daten nicht senden: %s',
+
+'email_smtp_error' => 'Folgender SMTP-Fehler trat auf: %s',
+
+'message_required' => 'Email Mitteilung ist erforderlich',
+
 );
 
-/* End of file email_lang.php */
-/* Location: ./system/user/language/deutsch/email_lang.php */
+// EOF
