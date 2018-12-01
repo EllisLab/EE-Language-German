4da8009840a582994c116596b3289a64b47f5e0a
Ralph Segert <git@segert.net>
2018-12-01 20:42:01 +0100

    Update imglib_lang.php

--- system/user/language/deutsch/imglib_lang.php
+++ system/user/language/deutsch/imglib_lang.php
@@ -1,25 +1,45 @@
 <?php
 
-$lang['imglib_source_image_required'] = 'You must specify a source image in your preferences.';
-$lang['imglib_gd_required'] = 'The GD image library is required for this feature.';
-$lang['imglib_gd_required_for_props'] = 'Your server must support the GD image library in order to determine the image properties.';
-$lang['imglib_unsupported_imagecreate'] = 'Your server does not support the GD function required to process this type of image.';
-$lang['imglib_gif_not_supported'] = 'GIF images are often not supported due to licensing restrictions.  You may have to use JPG or PNG images instead.';
-$lang['imglib_jpg_not_supported'] = 'JPG images are not supported.';
-$lang['imglib_png_not_supported'] = 'PNG images are not supported.';
-$lang['imglib_jpg_or_png_required'] = 'The image resize protocol specified in your preferences only works with JPEG or PNG image types.';
-$lang['imglib_copy_error'] = 'An error was encountered while attempting to replace the file.  Please make sure your file directory is writable.';
-$lang['imglib_rotate_unsupported'] = 'Image rotation does not appear to be supported by your server.';
-$lang['imglib_libpath_invalid'] = 'The path to your image library is not correct.  Please set the correct path in your image preferences.';
-$lang['imglib_image_process_failed'] = 'Image processing failed.  Please verify that your server supports the chosen protocol and that the path to your image library is correct.';
-$lang['imglib_rotation_angle_required'] = 'An angle of rotation is required to rotate the image.';
-$lang['imglib_writing_failed_gif'] = 'GIF image.';
-$lang['imglib_invalid_path'] = 'The path to the image is not correct.';
-$lang['imglib_copy_failed'] = 'The image copy routine failed.';
-$lang['imglib_missing_font'] = 'Unable to find a font to use.';
-$lang['imglib_save_failed'] = 'Unable to save the image.  Please make sure the image and file directory are writable.';
+$lang = array(
 
+'imglib_copy_error' => 'Beim Versuch, die Datei zu ersetzen, ist ein Fehler aufgetreten. Bitte stellen Sie sicher, dass Ihr Dateiverzeichnis schreibbar ist.',
 
-/* End of file imglib_lang.php */
-/* Location: ./system/user/language/deutsch/imglib_lang.php */
+'imglib_copy_failed' => 'Die Image-Kopierroutine ist fehlgeschlagen.',
 
+'imglib_gd_required' => 'Die GD-Bildbibliothek ist für diese Funktion erforderlich.',
+
+'imglib_gd_required_for_props' => 'Ihr Server muss die GD-Bildbibliothek unterstützen, um die Bildeigenschaften bestimmen zu können.',
+
+'imglib_gif_not_supported' => 'GIF-Bilder werden aufgrund von Lizenzbeschränkungen häufig nicht unterstützt. Möglicherweise müssen Sie stattdessen JPG- oder PNG-Bilder verwenden.',
+
+'imglib_image_process_failed' => 'Bildverarbeitung fehlgeschlagen Stellen Sie sicher, dass Ihr Server das ausgewählte Protokoll unterstützt und dass der Pfad zu Ihrer Bildbibliothek korrekt ist.',
+
+'imglib_invalid_path' => 'Der Pfad zum Bild ist nicht korrekt.',
+
+'imglib_jpg_not_supported' => 'JPG-Bilder werden nicht unterstützt.',
+
+'imglib_jpg_or_png_required' => 'Das in Ihren Voreinstellungen angegebene Bildgrößenänderungsprotokoll funktioniert nur mit JPEG- oder PNG-Bildtypen.',
+
+'imglib_libpath_invalid' => 'Der Pfad zu Ihrer Bildbibliothek ist nicht korrekt. Bitte legen Sie den richtigen Pfad in Ihren Bildeinstellungen fest.',
+
+'imglib_unsafe_config' => 'Ein Konfigurationselement enthält Zeichen, die für die Ausführung in einer Shell-Umgebung nicht sicher sind.',
+
+'imglib_missing_font' => 'Eine zu verwendende Schriftart konnte nicht gefunden werden.',
+
+'imglib_png_not_supported' => 'PNG-Bilder werden nicht unterstützt.',
+
+'imglib_rotate_unsupported' => 'Die Bildrotation scheint von Ihrem Server nicht unterstützt zu werden.',
+
+'imglib_rotation_angle_required' => 'Ein Drehwinkel ist erforderlich, um das Bild zu drehen.',
+
+'imglib_save_failed' => 'Das Bild kann nicht gespeichert werden. Stellen Sie sicher, dass das Image und das Dateiverzeichnis schreibbar sind.',
+
+'imglib_source_image_required' => 'Das Bild kann nicht gespeichert werden. Stellen Sie sicher, dass das Image und das Dateiverzeichnis schreibbar sind.',
+
+'imglib_unsupported_imagecreate' => 'Ihr Server unterstützt die für die Verarbeitung dieses Abbildtyps erforderliche GD-Funktion nicht.',
+
+'imglib_writing_failed_gif' => 'GIF-Bild.',
+
+);
+
+// EOF
