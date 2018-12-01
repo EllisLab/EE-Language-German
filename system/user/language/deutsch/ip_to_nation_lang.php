8443f85bd99ac95653dd482461006b00f51b2412
Ralph Segert <git@segert.net>
2018-12-01 20:43:00 +0100

    Update ip_to_nation_lang.php

--- system/user/language/deutsch/ip_to_nation_lang.php
+++ system/user/language/deutsch/ip_to_nation_lang.php
@@ -1,37 +1,15 @@
 <?php
+
 $lang = array(
 
+'ban_info' => 'Länder wählen, die ausgesperrt werden sollen. Ist ein Land ausgesperrt, kann ein Besucher mit entsprechender IP keine Kommentare verfassen oder die Email-/Tell-A-Friend-Formulare benutzen. Die Website ist für ihn aber sichtbar',
 
-'ip_to_nation_module_name' => 'IP zur Nation',
+'banlist' => 'Nationen-Sperrliste',
 
-'ip_to_nation_module_description' => 'Programm zur Länderzuweisung von IP-Addressen',
+'banlist_updated' => 'Sperrliste aktualisiert',
 
-'iptonation_missing' => 'Datei iptonation.php nicht gefunden. Bitte sicherstellen, dass alle Teile des Moduls auf dem Server liegen.',
+'banlist_updated_desc' => 'Die Sperrliste wurde erfolgreich aktualisiert.',
 
-'countryfile_missing' => 'Datei country.php im Ordner lib nicht gefunden.',
-
-'ip_search' => 'IP-Adressen-Suche',
-
-'ip_search_inst' => 'IP-Adresse eingeben, die für das betreffende Land bestimmt werden soll.',
-
-'ip_result' => 'Die eingegebene IP gehört zu folgendem Land:',
-
-'ip_not_valid' => 'Die angegebene IP ist keine gültige IP-Adresse.',
-
-'ip_not_found' => 'Keine Ergebnisse für die angegebene IP.',
-
-'cache_full' => 'Der Cache ist voll und muss manuell geleert werden, um fortzufahren.',
-
-'manage_banlist' => 'Verwaltung der ausgesperrten Länder',
-
- 'search_for_ip' => 'IP-Adresse',
-
-'search_for_ip_desc' => 'Geben Sie eine IP-Adresse ein, um das Land zu ermitteln, dem sie zugeordnet ist.',
-
-'no_countries' => 'Keine <b>Länder</b> gefunden.',
-
-'country' => 'Land',
-  
 'banned_countries' => 'Gesperrte Länder',
 
 'btn_save_banlist' => 'Sperrliste speichern',
@@ -40,47 +18,52 @@ $lang = array(
 
 'btn_searching' => 'Suche...',
 
-'countryfile_missing' => 'Die Datei country.php ist nicht in Ihrem "config"-Verzeichnis zu finden.',
+'cache_full' => 'Der Cache ist voll und muss manuell gelöscht werden, um fortfahren zu können.',
+
+'countryfile_missing' => 'Der Cache ist voll und muss manuell gelöscht werden, um fortfahren zu können.',
 
 'ip_address_located' => 'IP-Adresse lokalisiert',
 
 'ip_address_not_located' => 'Lokalisierung der IP-Addresse nicht möglich',
 
-'ban_info' => 'Länder wählen, die ausgesperrt werden sollen. Ist ein Land ausgesperrt, kann ein Besucher mit entsprechender IP keine Kommentare verfassen, Trackbacks setzen oder die Email-/Tell-A-Friend-Formulare benutzen. Die Website ist für ihn aber sichtbar.',
+'ip_db_connection_403' => '403 Verbindungsfehler: Bitte 24 Stunden warten, bevor die Datenbank erneut abgefragt wird.',
 
-'ban' => 'Sperren',
-
-'banlist' => 'Nationen-Sperrliste',
-
-'banlist_updated' => 'Sperrliste erfolgreich nachgeführt',
-
-'update_ips' => 'IP-Datenbank updaten',
-
-'update_info' => 'IP- und Ländercodeinformationen mit dem aktuellen Export von <a href="%d" title="Download hier">ip2nation.com</a> updaten',
-
-'update_blurb' => 'Für ein Update muss die ungezippte Datei mit den ip2nation-sql-Daten auf den Server gelegt werden und dann der Ort der Datei angegeben werden.',
-
-'update_data_provider' => 'Datenquelle: <a href="maxmind.com">maxmind.com</a>',
-
-'last_update' => 'Letztes Update:',
-
-'ip_db_updating' => 'Download des Updates',
+'ip_db_connection_error' => 'Keine Verbindung mit dem Daten-Provider möglich..',
 
 'ip_db_downloaded' => 'IP-Datenbank heruntergeladen. Entpacke ...',
+
+'ip_db_failed' => 'Unerlaubte Zeichen in der Import-Datei',
 
 'ip_db_unpacked' => 'IP-Datenbank entpackt. Füge Daten ein ...',
 
 'ip_db_updated' => 'IP-Datenbank-Update erfolgreich',
 
-'ip_db_failed' => 'Unerlaubte Zeichen in der Import-Datei',
+'ip_db_updating' => 'Download des Updates ...',
 
-'ip_db_connection_403' => '403 Verbindungsfehler: Bitte 24 Stunden warten, bevor die Datenbank erneut abgefragt wird.',
+'ip_not_found' => 'Keine Ergebnisse für die angegebene IP.',
 
-'ip_db_connection_error' => 'Keine Verbindung mit dem Daten-Provider möglich.',
+'ip_not_valid' => 'Die angegebene IP ist keine gültige IP-Adresse',
 
-'translate' => 'Update',
+'ip_result' => 'Die eingegebene IP gehört zu folgendem Land:',
 
-''=>''
+'ip_search' => 'IP-Adressen-Suche',
+
+'ip_to_nation_module_name' => 'IP zur Nation',
+
+'last_update' => 'Letztes Update: ',
+
+'manage_banlist' => 'Verwalten Sie Ihre Liste gesperrter Länder',
+
+'no_countries' => 'Keine <b>Länder</b> gefunden.',
+
+'search_for_ip' => 'Suchen Sie nach einer IP-Adresse',
+
+'search_for_ip_desc' => 'Geben Sie eine IP-Adresse ein, um das Land zu bestimmen, mit dem es verbunden ist.',
+
+'update_info' => 'Aktualisiert die IP- und Ländercode-Informationen. Datenquelle: <a href="%s" title="Data Provider" rel="external">maxmind.com</a>. </em><em><i>Die neuen Daten werden automatisch heruntergeladen und angewendet. Dies kann einige Minuten dauern und die Belastung Ihrer Datenbank erhöhen.</i></em>',
+
+'update_ips' => 'IP-Datenbank updaten',
+
 );
 
-// End of File
+// EOF
