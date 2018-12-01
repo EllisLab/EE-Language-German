dcb7a32363ab1dc08c748c9accd30e59471b0d70
Ralph Segert <git@segert.net>
2018-12-01 20:45:31 +0100

    Update logs_lang.php

--- system/user/language/deutsch/logs_lang.php
+++ system/user/language/deutsch/logs_lang.php
@@ -2,103 +2,158 @@
 
 $lang = array(
 
-'system_logs'			=> 'System-Logs',
+'system_logs' => 'System-Logs',
+
 
 /* Sidebar */
-'developer_log' 		=> 'Entwickler',
-'cp_log' 				=> 'Control Panel',
-'throttle_log' 			=> 'Throttling',
-'email_log' 			=> 'Email',
-'search_log' 			=> 'Suche',
+'all_consent_logs' => 'Alle %d Einwilligungs-Logs',
 
-'all_developer_logs' 	=> 'Alle %d Entwickler-Logs',
-'all_cp_logs' 			=> 'Alle %d Control Panel-Logs',
-'all_throttle_logs' 	=> 'Alle %d Throttling-Logs',
-'all_email_logs' 		=> 'Alle %d Email-Logs',
-'all_search_logs' 		=> 'Alle %d Such-Logs',
+'all_cp_logs' => 'Alle %d Control-Panel-Logs',
 
-'view_throttle_log' 	=> 'Throttling-Logs sehen',
-'view_search_log' 		=> 'Such-Logs sehen',
-'view_email_logs' 		=> 'Email-Logs sehen',
-'view_cp_log' 			=> 'Control Panel Zugangs-Logs sehen',
-'view_developer_log'	=> 'Entwickler-Logs sehen',
+'all_developer_logs' => 'Alle %d Entwickler-Logs',
 
-'logs_deleted'			=> 'Logs gelöscht',
-'logs_deleted_desc'		=> '<b>%d</b> Log(s) von <b>%s</b> Logs gelöscht',
+'all_email_logs' => 'Alle %d Email-Logs',
 
-'clear_cp_logs'			=> 'Alle CP Zugangs-Logs löschen',
+'all_search_logs' => 'Alle %d Such-Logs',
 
-'search_results' 		=> 'Suchresultate',
-'site_search' 			=> 'Site',
-'searched_in' 			=> 'Gesucht in',
-'search_terms' 			=> 'Suchbegriffe',
+'all_throttle_logs' => 'Alle %d Throttling Logs',
 
-'search_logs_button'	=> 'Such-Logs',
+'clear_cp_logs' => 'Alle Control-Panel-Zugangs-Logs löschen',
 
-'date_logged'			=> 'Datum gelogged',
+'consent_log' => 'Einwilligung',
 
-'no_control_panel_logs_found' => 'Keine <b>Control Panel-Logs</b> gefunden.',
+'cp_log' => 'Control-Panel',
+
+'date_logged' => 'Datum gelogged',
+
+'developer_log' => 'Entwickler',
+
+'email_log' => 'Email',
+
+'logs_deleted' => 'Logs gelöscht',
+
+'logs_deleted_desc' => '<b>%d</b> Log(s) von <b>%s</b> Logs gelöscht',
+
+'no_consent_logs_found' => 'Keine <b>Einwilligungs-Logs</b> gefunden.',
+
+'no_control_panel_logs_found' => 'Keine <b>Control-Panel-Logs</b> gefunden.',
+
+'search_log' => 'Suche',
+
+'search_logs_button' => 'Such-Logs',
+
+'search_results' => 'Suchergebnisse',
+
+'search_terms' => 'Suchbegriffe',
+
+'searched_in' => 'Gesucht in',
+
+'site_search' => 'Site',
+
+'throttle_log' => 'Throttling',
+
+'view_consent_log' => 'Einwillungs-Logs',
+
+'view_cp_log' => 'Control-Panel-Zugangs-Logs sehen',
+
+'view_developer_log' => 'Entwickler-Logs',
+
+'view_email_logs' => 'Email-Logs',
+
+'view_search_log' => 'Such-Logs',
+
+'view_throttle_log' => 'Throttling-Logs sehen',
+
 
 /* Developer Log */
-'no_developer_logs_found' => 'Keine <b>Entwickler-Logs</b> gefunden.',
-'clear_developer_logs'	=> 'Alle Entwickler-Logs gelöscht',
-'log_id'		=> 'Log-ID',
-'log_message'		=> 'Nachricht',
-'deprecated_function'	=> 'Veraltete Funktion %s benutzt',
-'deprecated_on_line'	=> 'von %s in Zeile %d.',
-'deprecated_since'		=> 'Veraltet seit %s.',
-'deprecated_use_instead'	=> 'Stattdessen %s benutzen.',
-'deprecated_template'	=> 'Von Template-Tag %s in %s.',
-'deprecated_snippets'	=> 'Dieser Tag wurde vielleicht von einem dieser Snippets benutzt: %s',
-'deprecated_hook'		=> 'Veralteter Extension Hook benutzt von den folgenden Extensions (Class::method) <ul>%s</ul>',
-'deprecation_detected'	=> 'Dies is ein Log für Entwickler, um Mitteilungen oder Warnungen dem Super-Admin zu melden.',
-'dev_log_view_report'	=> 'Report ansehen',
-'dev_log_help'			=> 'Was heißt das?',
-'deprecated_meaning'	=>
-'<p>Eine der zwei Sachen ist passiert:</p>
+'clear_developer_logs' => 'Alle Entwickler-Logs entfernen',
+
+'deprecated_function' => 'Veraltete Funktion %s benutzt',
+
+'deprecated_hook' => 'Veralteter Extension Hook benutzt von den folgenden Extensions (Class::method) <ul>%s</ul>',
+
+'deprecated_meaning' => '<p>Eine der zwei Sachen ist passiert:</p>
 <ol>
-	<li>Das System hat nativen Code oder ein Add-on entdeckt, das veralteten Code benutzt, der nicht mehr funktioninert oder das System beeinträchtigt. Das bedeutet, dass ein Add-On in dieser Installation einen Teil des ExpressionEngine Core-Codes benutzt, den EllisLab plant, in Zukunft aus dem System zu entfernen.</li>
-	<li>Eine Warnung oder Benachrichtigung wurde von nativem Code oder einem Add-On hervorgerufen, um dem Super-Admin gemeldet zu werden.</li>
+ <li>Das System hat nativen Code oder ein Add-on entdeckt, das veralteten Code benutzt, der nicht mehr funktioninert oder das System beeinträchtigt. Das bedeutet, dass ein Add-On in dieser Installation einen Teil des ExpressionEngine Core-Codes benutzt, den EllisLab plant, in Zukunft aus dem System zu entfernen.</li>
+ <li>Eine Warnung oder Benachrichtigung wurde von nativem Code oder einem Add-On hervorgerufen, um dem Super-Admin gemeldet zu werden.</li>
 </ol>
 <p>Um diese Benachrichtung abzuschalten, befolgen Sie einen der folgenden Punkte:</p>
 <ul>
-	<li>Den Entwickler, der sich um das System kümmert, um Hilfe bitten.</li>
-	<li>Sicherstellen, daß überall die neusten Versionen aller installierten Add-Ons laufen.</li>
-	<li>Den oder die Entwickler des betreffenden Add-Ons kontaktieren, um sicherzustellen, dass sie sich des Problems bewusst sind und sich um die Behebung des Problems bemühen.</li>
+ <li>Den Entwickler, der sich um das System kümmert, um Hilfe bitten.</li>
+ <li>Sicherstellen, daß überall die neusten Versionen aller installierten Add-Ons laufen.</li>
+ <li>>Den oder die Entwickler des betreffenden Add-Ons kontaktieren, um sicherzustellen, dass sie sich des Problems bewusst sind und sich um die Behebung des Problems bemühen.</li>
 </ul>',
 
+'deprecated_on_line' => 'von %s in Zeile %d.',
+
+'deprecated_since' => 'Veraltet seit %s.',
+
+'deprecated_snippets' => 'Dieser Tag wurde vielleicht von einem dieser Snippets benutzt: %s',
+
+'deprecated_template' => 'Von Template-Tag %s in %s.',
+
+'deprecated_use_instead' => 'Stattdessen %s benutzen.',
+
+'deprecation_detected' => 'Dies is ein Log für Entwickler, um Mitteilungen oder Warnungen dem Super-Admin zu melden.',
+
+'dev_log_help' => 'Was heißt das?',
+
+'dev_log_view_report' => 'Report ansehen',
+
+'log_id' => 'Log-ID',
+
+'log_message' => 'Nachricht',
+
+'no_developer_logs_found' => 'Keine <b>Entwickler-Logs</b> gefunden.',
+
+
 /* Email Log */
-'no_email_logs_found'	=> 'Keine <b>Email-Logs</b> gefunden.',
-'sent_to'				=> 'Gesendete Email an',
-'subject'				=> 'Subject',
-'clear_email_logs'		=> 'Lösche alle Email-Logs',
+'clear_email_logs' => 'Lösche alle Email-Logs',
+
+'no_email_logs_found' => 'Keine <b>Email-Logs</b> gefunden.',
+
+'sent_to' => 'Gesendete Email an',
+
+'subject' => 'Subject',
+
 
 /* Search Log */
-'no_search_logs_found'	=> 'Keine <b>Such-Logs</b> gefunden.',
-'clear_search_logs'		=> 'Lösche alle Such-Logs',
-'searched_for'			=> 'Gesucht nach',
+'clear_search_logs' => 'Lösche alle Such-Logs',
+
+'no_search_logs_found' => 'Keine <b>Such-Logs</b> gefunden.',
+
+'searched_for' => 'Gesucht nach',
+
 
 /* Throttle Log */
-'no_throttling_logs_found'	=> 'Keine <b>Throttling-Logs</b> gefunden.',
-'enable_throttling'		=> 'Throttling anschalten',
-'throttling_disabled'	=> '<b>Throttling</b> abgeschaltet',
-'front_end_requests'	=> 'Frontend-Anfragen',
-'last_activity' 		=> 'Letzte Aktivität',
-'no_throttle_logs' 		=> 'Keine IPs werden zur Zeit vom System beschränkt.',
-'blacklist_all_ips'		=> 'Alle IPs auf die schwarze Liste setzen',
-'clear_throttle_logs'	=> 'Lösche alle Throttling-Logs',
+'blacklist_all_ips'	=> 'Alle IPs auf die schwarze Liste setzen',
+
+'clear_throttle_logs' => 'Lösche alle Throttling-Logs',
+
+'enable_throttling' => 'Throttling anschalten',
+
+'front_end_requests' => 'Frontend-Anfragen',
+
+'last_activity' => 'Letzte Aktivität',
+
+'no_throttle_logs' => 'Keine IPs werden zur Zeit vom System beschränkt.',
+
+'no_throttling_logs_found' => 'Keine <b>Throttling-Logs</b> gefunden.',
+
+'throttling_disabled' => '<b>Throttling</b> abgeschaltet',
+
 
 /* Filters */
-'filters'				=> 'Filter',
-'filter_by_username'	=> 'Filter Benutzername',
-'filter_by_site'		=> 'Filter Sites',
-'date'					=> 'Datum',
-'custom_date'			=> 'Eigenes Datum',
-'show'					=> 'Zeigen',
-'custom_limit'			=> 'Eigenes Limit',
+'custom_date' => 'Eigenes Datum',
 
-''=>''
+'custom_limit' => 'Eigenes Limit',
+
+'date' => 'Datum',
+
+'filter_by_site' => 'Filter Sites',
+
+'filter_by_username' => 'Filter Benutzername',
+
 );
 
-/* End of file logs_lang.php */
-/* Location: ./system/user/language/deutsch/logs_lang.php */
+// EOF
