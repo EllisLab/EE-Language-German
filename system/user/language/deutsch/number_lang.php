79d8dc2fbd8675699fee80a657c6e4f8bcb144ab
Ralph Segert <git@segert.net>
2018-12-29 22:20:03 +0100

    EE 4.0.6 German

--- system/user/language/deutsch/number_lang.php
+++ system/user/language/deutsch/number_lang.php
@@ -1,10 +1,17 @@
 <?php
 
-$lang['terabyte_abbr'] = '<abbr title="Terabyte">TB</abbr>';
-$lang['gigabyte_abbr'] = '<abbr title="Gigabyte">GB</abbr>';
-$lang['megabyte_abbr'] = '<abbr title="Megabyte">MB</abbr>';
-$lang['kilobyte_abbr'] = '<abbr title="Kilobyte">KB</abbr>';
-$lang['bytes'] = "Bytes";
+$lang = array(
 
-/* End of file number_lang.php */
-/* Location: ./system/language/english/number_lang.php */
+'bytes' => 'Bytes',
+
+'gigabyte_abbr' => '<abbr title="Gigabyte">GB</abbr>',
+
+'kilobyte_abbr' => '<abbr title="Kilobyte">KB</abbr>',
+
+'megabyte_abbr' => '<abbr title="Megabyte">MB</abbr>',
+
+'terabyte_abbr' => '<abbr title="Terabyte">TB</abbr>',
+
+);
+
+// EOF
