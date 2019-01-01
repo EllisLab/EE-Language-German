136037ce4d3292549654b3b04f8e53c2382911bd
Ralph Segert <git@segert.net>
2019-01-01 16:10:12 +0100

    EE 4.0.6 German

--- system/user/language/deutsch/unit_test_lang.php
+++ system/user/language/deutsch/unit_test_lang.php
@@ -1,25 +1,44 @@
 <?php
 
-$lang['ut_test_name']		= 'Test Name';
-$lang['ut_test_datatype']	= 'Test Datentyp';
-$lang['ut_res_datatype']	= 'Erwarteter Datentyp';
-$lang['ut_result']			= 'Resultat';
-$lang['ut_undefined']		= 'Undefinierter Test Name';
-$lang['ut_file']			= 'Dateiname';
-$lang['ut_line']			= 'Zeilennummer';
-$lang['ut_passed']			= 'Bestanden';
-$lang['ut_failed']			= 'Fehlgeschlagen';
-$lang['ut_boolean']			= 'Boolean';
-$lang['ut_integer']			= 'Integer';
-$lang['ut_float']			= 'Float';
-$lang['ut_double']			= 'Float'; // can be the same as float
-$lang['ut_string']			= 'String';
-$lang['ut_array']			= 'Array';
-$lang['ut_object']			= 'Object';
-$lang['ut_resource']		= 'Ressource';
-$lang['ut_null']			= 'Null';
-$lang['ut_notes']			= 'Notes';
+$lang = array(
 
+'ut_array' => 'Array',
 
-/* End of file unit_test_lang.php */
-/* Location: ./system/language/english/unit_test_lang.php */
+'ut_boolean' => 'Boolean',
+
+'ut_double' => 'Float', // kann das gleiche wie Float sein
+'ut_failed' => 'Fehlgeschlagen',
+
+'ut_file' => 'Dateiname',
+
+'ut_float' => 'Float',
+
+'ut_integer' => 'Integer',
+
+'ut_line' => 'Zeilennummer',
+
+'ut_notes' => 'Notes',
+
+'ut_null' => 'Null',
+
+'ut_object' => 'Object',
+
+'ut_passed' => 'Bestanden',
+
+'ut_res_datatype' => 'Erwarteter Datentyp',
+
+'ut_resource' => 'Ressource',
+
+'ut_result' => 'Ergebnis',
+
+'ut_string' => 'String',
+
+'ut_test_datatype' => 'Test Datentyp',
+
+'ut_test_name' => 'Testname',
+
+'ut_undefined' => 'Undefinierter Testname',
+
+);
+
+// EOF
